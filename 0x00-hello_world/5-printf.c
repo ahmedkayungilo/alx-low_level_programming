2df5ed3c8a5ad6617697aac384130c4361d6e52f
ahmedkayungilo <ahmedkayungilo@gmail.com>
2022-12-08 00:22:53 -0800

    Add 5-printf-c source file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - This program simply prints specified string
+ *
+ * Description: This program prints the string "with proper grammar, but the outcome is a piece of art,"
+ * using the printf() functions
+ * Return: The program always returns 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
