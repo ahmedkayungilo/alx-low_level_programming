f30243cd95cedc5e9e5cb2d939f08f4619cfdc21
ahmedkayungilo <ahmedkayungilo@gmail.com>
2022-12-08 02:33:35 -0800

    Update 101-quote.c to match stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,8 +12,7 @@
 
 int main(void)
 {
-	write(1, "and that piece of art its useful\" - Dora Kopar, 2015-10-19\n", 59);
-	write(2, "[Anything]", 10);
+	write(1, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
 	return (1);
 }
 
