949022d50b702a3be623e6fb78601f8772794e09
ahmedkayungilo <ahmedkayungilo@gmail.com>
2022-12-08 00:03:13 -0800

    Add 4-puts.c source file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * This is a program that prints the string
+ * "Programming is like building a multilingual puzzle
+ * usiing the puts funtion
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
+
