4ff08831948dbf5a314c407595a284fd402fdf82
ahmedkayungilo <ahmedkayungilo@gmail.com>
2022-12-08 02:08:57 -0800

    Add 101-quote.c source file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - Prints a specified quote
+ *
+ * Description: This program prints the quote
+ * "and that piece of art is useful" - Dora Kopar, 2015-10-19
+ * followed by a new line
+ * Return: Always returns 1
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art its useful\" - Dora Kopar, 2015-10-19\n", 59);
+	return 1;
+}
+
